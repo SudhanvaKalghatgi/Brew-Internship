643781153551218492c20071a457daa26ad28833
Sudhanva Kalghatgi <sudhanvakalghatgi3@gmail.com>
2026-03-03 19:39:43 +0530

    Frontend refactored

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://brew-internship.onrender.com/api/v1" || "http://localhost:5000/api/v1",
+    baseURL: "https://brew-internship.onrender.com/api/v1",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
